1625613ad4ffa1386d71c81ec37b45a659fc6edc
wanty33 <wanty047@gmail.com>
2024-11-24 19:29:07 +0800

    update file java main, jelah kucing, about us

--- app/src/main/java/com/aswanti/aplikasikatalogkucing/MainActivity.java
+++ app/src/main/java/com/aswanti/aplikasikatalogkucing/MainActivity.java
@@ -1,13 +1,16 @@
 package com.aswanti.aplikasikatalogkucing;
 
+import android.content.Intent;
 import android.os.Bundle;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.view.MenuItem;
 import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,5 +28,25 @@ public class MainActivity extends AppCompatActivity {
                 return insets;
             });
         }
+
+        BottomNavigationView navView = findViewById(R.id.bottomNav);
+        navView.setOnItemSelectedListener(this::onNavigationItemSelected);
+    }
+
+    private boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.nav_home) {
+            // Stay in MainActivity
+            return true;
+        } else if (id == R.id.nav_food) {
+            Intent intent = new Intent(this, JelajahiKucingActivity.class);
+            startActivity(intent);
+            return true;
+        } else if (id == R.id.nav_account) {
+            Intent intent = new Intent(this, AboutUsActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return false;
     }
 }
