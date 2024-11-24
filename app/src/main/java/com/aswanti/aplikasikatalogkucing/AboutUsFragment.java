cbf4af35ed59cf97f868de37336f64ed7e3dca9a
wanty33 <wanty047@gmail.com>
2024-11-24 19:32:54 +0800

    fixing files java main, jelah kucing, about us

--- app/src/main/java/com/aswanti/aplikasikatalogkucing/AboutUsFragment.java
+++ app/src/main/java/com/aswanti/aplikasikatalogkucing/AboutUsFragment.java
@@ -1,14 +1,25 @@
 package com.aswanti.aplikasikatalogkucing;
 
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
 
-public class AboutUsFragment extends AppCompatActivity {
+public class AboutUsFragment extends Fragment {
+
+    public AboutUsFragment() {
+        // Required empty public constructor
+    }
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_about_us);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_about_us, container, false);
 
-        findViewById(R.id.btnBack).setOnClickListener(v -> finish());
+        view.findViewById(R.id.btnBack).setOnClickListener(v -> getActivity().finish());
+
+        return view;
     }
 }
--- app/src/main/java/com/aswanti/aplikasikatalogkucing/FragmentJelajahiKucing.java
+++ app/src/main/java/com/aswanti/aplikasikatalogkucing/FragmentJelajahiKucing.java
@@ -1,14 +1,25 @@
 package com.aswanti.aplikasikatalogkucing;
 
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
 
-public class FragmentJelajahiKucing extends AppCompatActivity {
+public class FragmentJelajahiKucing extends Fragment {
+
+    public FragmentJelajahiKucing() {
+        // Required empty public constructor
+    }
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_jelajahi_kucing);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_jelajahi_kucing, container, false);
 
-        findViewById(R.id.btnBack).setOnClickListener(v -> finish());
+        view.findViewById(R.id.btnBack).setOnClickListener(v -> getActivity().finish());
+
+        return view;
     }
 }
